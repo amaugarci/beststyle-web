80d502c128fcf1f2ec5d9a7555049e88533b7461
Amaury Garcia <amaugarci@outlook.com>
2023-03-14 15:28:41 +0800

    添加部署脚本

--- deployments/finished.sh
+++ deployments/finished.sh
@@ -6,6 +6,6 @@ if [ "${EVN}" = "pre" ];then
     docker tag pre-bourse-web:${VERSION}-${BUILD_ID} docker.biyong001.com/pre/bourse-web:${VERSION}-${BUILD_ID}
     docker push docker.biyong001.com/pre/bourse-web:${VERSION}-${BUILD_ID}
 else
-    docker tag bourse-web:${VERSION}-${BUILD_ID} docker.biyong001.com/casino/bourse-web:${VERSION}-${BUILD_ID}
+    docker tag bourse-web:${VERSION}-${BUILD_ID} docker.biyong001.com/bourse/bourse-web:${VERSION}-${BUILD_ID}
     docker push docker.biyong001.com/bourse/bourse-web:${VERSION}-${BUILD_ID}
 fi
