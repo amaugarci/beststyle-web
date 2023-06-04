687310f40a6edc3cea95f357496196423335da01
Amaury Garcia <amaugarci@outlook.com>
2023-06-04 14:59:28 +0800

    test

--- deployments/finished.sh
+++ deployments/finished.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-docker login  -u admin -p Fh7FSgEQ6hMn8MB7Q4URnh76eEDdIXAg https://docker.biyong001.com/
+docker login  -u bourse -p bourseOcRqW9852 https://docker.flct.io/
 
 if [ "${EVN}" = "pre" ];then
-    docker tag pre-learn-web:${VERSION}-${BUILD_ID} docker.biyong001.com/pre/learn-web:${VERSION}-${BUILD_ID}
-    docker push docker.biyong001.com/pre/learn-web:${VERSION}-${BUILD_ID}
+    docker tag pre-learn-web:${VERSION}-${BUILD_ID} docker.flct.io/bourse/pre-learn-web:${VERSION}-${BUILD_ID}
+    docker push docker.flct.io/bourse/pre-learn-web:${VERSION}-${BUILD_ID}
 else
-    docker tag learn-web:${VERSION}-${BUILD_ID} docker.biyong001.com/learn/learn-web:${VERSION}-${BUILD_ID}
-    docker push docker.biyong001.com/learn/learn-web:${VERSION}-${BUILD_ID}
+    docker tag learn-web:${VERSION}-${BUILD_ID} docker.flct.io/bourse/learn-web:${VERSION}-${BUILD_ID}
+    docker push docker.flct.io/bourse/learn-web:${VERSION}-${BUILD_ID}
 fi
